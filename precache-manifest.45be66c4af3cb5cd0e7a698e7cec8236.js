35907c9db747eaebc129bc6dcd2fda3eabb0af10
Hamidreza Hanafi <hamidreza.hanafi@siemens.com>
2020-01-27 16:00:39 -0400

    Updates

---
+++ precache-manifest.45be66c4af3cb5cd0e7a698e7cec8236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a252698f4d7385eb5ecac25029c939bb",
+    "url": "/react-thermostat/index.html"
+  },
+  {
+    "revision": "a1ad82c2a898473ab729",
+    "url": "/react-thermostat/static/js/2.e329272c.chunk.js"
+  },
+  {
+    "revision": "8a0e15de72d9fdc9e774bdb41f5189ae",
+    "url": "/react-thermostat/static/js/2.e329272c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfa0ffe44758b68df86b",
+    "url": "/react-thermostat/static/js/main.782d39a4.chunk.js"
+  },
+  {
+    "revision": "c06044ed6611385a768b",
+    "url": "/react-thermostat/static/js/runtime-main.e768d11b.js"
+  }
+]);
